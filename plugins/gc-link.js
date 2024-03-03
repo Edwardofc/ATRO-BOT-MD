57871c675635a1653f50944bf664a270b38c641a
Edwardofc <150191327+Edwardofc@users.noreply.github.com>
2024-03-02 23:47:51 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, args}) => {
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
       title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-      body: '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝',
+      body: 'Atro - 𝙱𝚘𝚝',
       previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
       sourceUrl: `https://github.com/Edwardofc/ATRO-BOT`}}});
 };
