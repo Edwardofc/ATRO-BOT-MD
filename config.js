3d2f0aa0ef0114268d6377a0b6c646aed3e71f42
Edwardofc <150191327+Edwardofc@users.noreply.github.com>
2024-03-02 21:31:54 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nThe Mystic - Bot`;
+global.wm2 = `${dia} ${fecha}\nThe Atro - Bot`;
 global.gt = 'ATRO : BOT';
 global.atrobot = 'ATRO-BOT';
 global.md = 'https://github.com/Edwardofc/ATRO-BOT';
