bcefe7971ea0a0b85713e6fb7df3d1c0772cf89f
Edwardofc <150191327+Edwardofc@users.noreply.github.com>
2024-03-02 21:50:29 -0500

    Actualizar random-itzy.js

--- plugins/random-itzy.js
+++ plugins/random-itzy.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const handler = async (m, {conn, args, usedPrefix, command}) => {
   const res = (await axios.get(`https://raw.githubusercontent.com/Edwardofc/ATRO-BOT-MD/master/atro.sh/JSON/itzy.json`)).data;
   const atro = await res[Math.floor(res.length * Math.random())];
-  conn.sendFile(m.chat, mystic, 'error.jpg', `_${command}_`, m);
+  conn.sendFile(m.chat, atro, 'error.jpg', `_${command}_`, m);
 };
 // conn.sendButton(m.chat, `_${command}_`, author, mystic, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
 handler.help = ['itzy', 'kpopitzy'];
