587f242eed42bea652e98d366afd51a12575428f
Edwardofc <150191327+Edwardofc@users.noreply.github.com>
2024-03-02 21:51:07 -0500

    Actualizar random-itzy.js

--- plugins/random-itzy.js
+++ plugins/random-itzy.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
   const atro = await res[Math.floor(res.length * Math.random())];
   conn.sendFile(m.chat, atro, 'error.jpg', `_${command}_`, m);
 };
-// conn.sendButton(m.chat, `_${command}_`, author, mystic, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
+// conn.sendButton(m.chat, `_${command}_`, author, atro, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
 handler.help = ['itzy', 'kpopitzy'];
 handler.tags = ['internet'];
 handler.command = /^(itzy|kpopitzy)$/i;
