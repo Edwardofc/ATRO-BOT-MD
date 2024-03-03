2aa5a662e66467d524776f3fc03ab6c43e3fae9b
Edwardofc <150191327+Edwardofc@users.noreply.github.com>
2024-03-02 22:52:39 -0500

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -1091,7 +1091,7 @@ export async function handler(chatUpdate) {
             }
           }*/
           const md5c = fs.readFileSync('./plugins/' + m.plugin);
-          fetch('https://themysticbot.cloud:2083/error', {
+          fetch('https://atrobot.cloud:2083/error', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({number: conn.user.jid, plugin: m.plugin, command: `${m.text}`, reason: format(e), md5: mddd5(md5c)}),
