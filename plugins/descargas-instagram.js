2a1e9044548a6de2f3b8dee31445446462b7f9bd
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-06 07:27:43 -0500

    AtroBot

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -1,56 +1,58 @@
- import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 import axios from 'axios';
-import cheerio from 'cheerio'; 
-import fg from 'api-dylux'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `✳️ ejemplo ${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
-
-   let res = await igdl(args[0])
-    for (let result of res.data) {
-    conn.sendFile(m.chat, result.url, 'igdl.mp4', ``, m)
+import instagramGetUrl from 'instagram-url-direct';
+import {instagram} from '@xct007/frieren-scraper';
+import {instagramdl} from '@bochilteam/scraper';
+const handler = async (m, {conn, args, command, usedPrefix}) => {
+  if (!args[0]) throw `⚠️ _Ingrese Un Enlace De Instagram_\n\n*Ejemplo:*\n*${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link`;
+  m.reply(`*⚡️ ᴅᴇsᴄᴀʀɢᴀɴᴅᴏ sᴜ ᴠɪᴅᴇᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴘᴏʀ ғᴀᴠᴏʀ, ᴇsᴛᴇ ᴘʀᴏᴄᴇsᴏ ᴘᴜᴇᴅᴇ ᴅᴜʀᴀʀ ᴇɴᴛʀᴇ 2 ʏ 10 ᴍɪɴᴜᴛᴏs ᴅᴇᴘᴇɴᴅɪᴇɴᴅᴏ ᴅᴇ ʟᴀ ᴅᴜʀᴀᴄɪᴏɴ ᴅᴇʟ ᴠɪᴅᴇᴏ...*`);
+  try {
+    const apiUrll = `https://api.betabotz.org/api/download/igdowloader?url=${encodeURIComponent(args[0])}&apikey=bot-secx3`;
+    const responsel = await axios.get(apiUrll);
+    const resultl = responsel.data;
+    for (const item of resultl.message) {
+      const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${item.thumbnail}`)).text();
+      const tXXxt = `🔗 *Url:* ${shortUrRRl}`.trim();
+      conn.sendFile(m.chat, item._url, null, tXXxt, m);
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+    }
+  } catch {    
+  try {
+    const datTa = await instagram.v1(args[0]);
+    for (const urRRl of datTa) {
+      const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+      const tXXxt = `🔗 *Url:* ${shortUrRRl}`.trim();
+      conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m);
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+    }
+  } catch {
+      try {
+        const resultss = await instagramGetUrl(args[0]).url_list[0];
+        const shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+        const txt2 = `🔗 *Url:* ${shortUrl2}`.trim();
+        await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m);
+      } catch {
+        try {
+          const resultssss = await instagramdl(args[0]);
+          const shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+          const txt4 = `🔗 *Url:* ${shortUrl3}`.trim();
+          for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m);
+        } catch {
+          try {
+            const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`);
+            const json = await human.json();
+            const videoig = json.result;
+            const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
+            const txt1 = `🔗 *𝚄𝚁𝙻:* ${shortUrl1}`.trim();
+            await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m);
+          } catch {
+            throw `*𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`;
+          }
+        }
+      }
+    }
   }
-}
-handler.help = ['instagram <link ig>']
-handler.tags = ['dl']
-handler.command = ['ig', 'igdl', 'instagram', 'igimg', 'igvid'] 
-handler.diamond = true
-
-export default handler 
-
-
-async function igdl(url) {
-   try {
-      const response = await axios.post("https://saveig.app/api/ajaxSearch", new URLSearchParams({ q: url, t: "media", lang: "en" }).toString(), {
-         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Origin': 'https://saveig.app/en',
-            'Referer': 'https://saveig.app/en',
-            'Referrer-Policy': 'strict-origin-when-cross-origin',
-            'User-Agent': 'PostmanRuntime/7.31.1'
-         }
-      });
-
-      const $ = cheerio.load(response.data.data);
-      const data = $('div.download-items__btn').map((i, e) => {
-         const type = $(e).find('a').attr('href').match('.jpg') ? 'image' : 'video';
-         const url = $(e).find('a').attr('href');
-         return {
- type, 
-url
- };
-      }).get();
-
-      return {
-         status: data.length > 0,
-         data
-      };
-   } catch (error) {
-      
-      return {
-         status: false,
-         msg: error.message
-      };
-   }
-} 
-
+};
+handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i;
+handler.register = true
+export default handler;
