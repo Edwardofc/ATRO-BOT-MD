ce5a39371d14b3cb1bb98040659d0835b736f60d
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-03-05 22:08:10 -0300

    Update descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -1,70 +1,56 @@
-import fetch from 'node-fetch';
+ import fetch from 'node-fetch'
 import axios from 'axios';
-import instagramGetUrl from 'instagram-url-direct';
-import {instagram} from '@xct007/frieren-scraper';
-import {instagramdl} from '@bochilteam/scraper';
-import instagramDl from '@sasmeee/igdl';
-import {fileTypeFromBuffer} from 'file-type';
-const handler = async (m, {conn, args, command, usedPrefix}) => {
-  if (!args[0]) throw `_*< DESCARGAS - INSTAGRAM />*_\n\n*[ ℹ️ ] Ingrese un enlace de Instagram.*\n\n*[ 💡 ] Ejemplo:* _${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link_`;
-  m.reply(global.wait);
-  try {
-const img = await instagramDl(args[0]);
-for (let i = 0; i < img.length; i++) {
-    const bufferInfo = await getBuffer(img[i].download_link);
-        if (bufferInfo.detectedType.mime.startsWith('image/')) {
-            await conn.sendMessage(m.chat, {image: {url: img[i].download_link}}, {quoted: m});
-        } else if (bufferInfo.detectedType.mime.startsWith('video/')) {
-            await conn.sendMessage(m.chat, {video: {url: img[i].download_link }}, {quoted: m});
-        }
-}
-  } catch {   
-  try {
-    const datTa = await instagram.download(args[0]);
-    for (const urRRl of datTa) {
-      const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-      const tXXxt = `_*< DESCARGAS - INSTAGRAM />*_\n\n▢ *URL:* _${shortUrRRl}_`.trim();
-      conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m);
-      await new Promise((resolve) => setTimeout(resolve, 10000));
-    }
-  } catch {
-      try {
-        const resultss = await instagramGetUrl(args[0]).url_list[0];
-        const shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-        const txt2 = `_*< DESCARGAS - INSTAGRAM />*_\n\n▢ *URL:* _${shortUrl2}_`.trim();
-        await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m);
-      } catch {
-        try {
-          const resultssss = await instagramdl(args[0]);
-          const shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-          const txt4 = `_*< DESCARGAS - INSTAGRAM />*_\n\n▢ *URL:* _${shortUrl3}_`.trim();
-          for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m);
-        } catch {
-          try {
-            const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`);
-            const json = await human.json();
-            const videoig = json.result;
-            const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-            const txt1 = `_*< DESCARGAS - INSTAGRAM />*_\n\n▢ *URL:* _${shortUrl1}_`.trim();
-            await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m);
-          } catch {
-            throw `_*< DESCARGAS - INSTAGRAM />*_\n\n*[ ℹ️ ] Ocurrió un error. Por favor, inténtalo de nuevo más tarde.*`;
-          }
-        }
-      }
-    }
-  }
-};
-handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i;
-export default handler;
+import cheerio from 'cheerio'; 
+import fg from 'api-dylux'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `✳️ ejemplo ${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
 
-const getBuffer = async (url, options) => {
-    options = options || {};
-    const res = await axios({method: 'get', url, headers: {'DNT': 1, 'Upgrade-Insecure-Request': 1}, ...options, responseType: 'arraybuffer'});
-    const buffer = Buffer.from(res.data, 'binary');
-    const detectedType = await fileTypeFromBuffer(buffer);
-    if (!detectedType || (detectedType.mime !== 'image/jpeg' && detectedType.mime !== 'image/png' && detectedType.mime !== 'video/mp4')) {
-        return null;
-    }
-    return { buffer, detectedType };
-};
+   let res = await igdl(args[0])
+    for (let result of res.data) {
+    conn.sendFile(m.chat, result.url, 'igdl.mp4', ``, m)
+  }
+}
+handler.help = ['instagram <link ig>']
+handler.tags = ['dl']
+handler.command = ['ig', 'igdl', 'instagram', 'igimg', 'igvid'] 
+handler.diamond = true
+
+export default handler 
+
+
+async function igdl(url) {
+   try {
+      const response = await axios.post("https://saveig.app/api/ajaxSearch", new URLSearchParams({ q: url, t: "media", lang: "en" }).toString(), {
+         headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Origin': 'https://saveig.app/en',
+            'Referer': 'https://saveig.app/en',
+            'Referrer-Policy': 'strict-origin-when-cross-origin',
+            'User-Agent': 'PostmanRuntime/7.31.1'
+         }
+      });
+
+      const $ = cheerio.load(response.data.data);
+      const data = $('div.download-items__btn').map((i, e) => {
+         const type = $(e).find('a').attr('href').match('.jpg') ? 'image' : 'video';
+         const url = $(e).find('a').attr('href');
+         return {
+ type, 
+url
+ };
+      }).get();
+
+      return {
+         status: data.length > 0,
+         data
+      };
+   } catch (error) {
+      
+      return {
+         status: false,
+         msg: error.message
+      };
+   }
+} 
+
